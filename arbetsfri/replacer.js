45c940fe538fbe7a744c834eb308155cdc8e3fea
Olle S <o.serrander@gmail.com>
2015-06-29 23:48:56 +0200

    initial commit

---
+++ arbetsfri/replacer.js
@@ -0,0 +1,4 @@
+document.documentElement.innerHTML = document.documentElement.innerHTML.replace(new RegExp("arbetslös", "g"), "arbetsfri");
+document.documentElement.innerHTML = document.documentElement.innerHTML.replace(new RegExp("Arbetslös", "g"), "Arbetsfri");
+
+
